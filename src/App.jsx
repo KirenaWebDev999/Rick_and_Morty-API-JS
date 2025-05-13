a2248363c389ea0e16a2dba7c95c12a54659c279
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-13 23:00:47 +0200

    added HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import './App.css'
+import {HomePage} from "./pages/HomePage/HomePage.jsx";
+
 
 function App() {
-    return <div>Hello World!</div>
+    return (
+        <div>
+            <HomePage />
+        </div>
+    )
 }
 
 export default App
