a1915aa24cb145a396124ad981375757b5230602
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-14 01:04:24 +0200

    added styles

--- src/common/components/Header/Header.jsx
+++ src/common/components/Header/Header.jsx
@@ -1,17 +1,18 @@
 import logo from '../../../assets/img/logo.png'
 import {NavLink} from "react-router";
+import s from './Header.module.css'
 
 export const Header = () => {
     return (
-        <>
+        <div className={s.container}>
             <NavLink to="/">
-                <img src={logo} alt="logotype"/>
+                <img className={s.logo} src={logo} alt="logotype"/>
             </NavLink>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/characters">Characters</NavLink>
             <NavLink to="/locations">Locations</NavLink>
             <NavLink to="/episodes">Episodes</NavLink>
-        </>
+        </div>
     );
 };
 
