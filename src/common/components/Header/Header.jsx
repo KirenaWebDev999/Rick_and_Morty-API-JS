7a716983f0a07fb291b8555fdd08edb3a9b48b70
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-13 23:50:40 +0200

    added Navigation for Header

--- src/common/components/Header/Header.jsx
+++ src/common/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import logo from '../../../assets/img/logo.png'
+import {NavLink} from "react-router";
 
 export const Header = () => {
     return (
-        <div>
-            <img src={logo} alt="logotype"/>
-        </div>
+        <>
+            <NavLink to="/">
+                <img src={logo} alt="logotype"/>
+            </NavLink>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/characters">Characters</NavLink>
+            <NavLink to="/locations">Locations</NavLink>
+            <NavLink to="/episodes">Episodes</NavLink>
+        </>
     );
 };
 
