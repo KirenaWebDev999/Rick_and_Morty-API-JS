d800caae0efdce72c7f7c7525ef09357a35a13df
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-14 00:36:46 +0200

    added styles for linkWrapper and linkButtons

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,10 +6,10 @@ export const HomePage = () => {
     return (
         <>
             <h1 className={`pageTitle ${s.title}`}>The Rick and Morty</h1>
-            <div>
-                <NavLink to="/characters">Characters</NavLink>
-                <NavLink to="/locations">Locations</NavLink>
-                <NavLink to="/episodes">Episodes</NavLink>
+            <div className={s.linkWrapper}>
+                <NavLink to="/characters" className={'linkButton'}>Characters</NavLink>
+                <NavLink to="/locations" className={'linkButton'}>Locations</NavLink>
+                <NavLink to="/episodes" className={'linkButton'}>Episodes</NavLink>
             </div>
         </>
     );
