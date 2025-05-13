af700a7d80fcd8b78bd0aa123b23d2672798c809
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-13 23:07:23 +0200

    added CharacterPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import {HomePage} from "./pages/HomePage/HomePage.jsx";
 import {LocationPage} from "./pages/LocationPage/LocationPage.jsx";
 import {EpisodePage} from "./pages/EpisodePage/EpisodePage.jsx";
+import {CharacterPage} from "./pages/CharacterPage/CharacterPage.jsx";
 
 
 function App() {
     return (
         <div>
             <HomePage/>
+            <CharacterPage/>
             <LocationPage/>
             <EpisodePage/>
         </div>
