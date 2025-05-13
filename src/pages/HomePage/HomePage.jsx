477e08118acf8389e7cf86b6e3a2815a3aded069
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-14 00:30:57 +0200

    added global+local styles for title

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ import s from './HomePage.module.css'
 export const HomePage = () => {
     return (
         <>
-            <h1 className={s.title}>The Rick and Morty</h1>
+            <h1 className={`pageTitle ${s.title}`}>The Rick and Morty</h1>
             <div>
                 <NavLink to="/characters">Characters</NavLink>
                 <NavLink to="/locations">Locations</NavLink>
