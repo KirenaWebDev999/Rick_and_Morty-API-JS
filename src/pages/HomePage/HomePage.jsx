7ebfced123ee90d1cc25f0dfc1fc3482e50a0583
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-13 23:43:08 +0200

    added NavLinks

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,15 @@
+import {NavLink, Route} from "react-router";
+import {CharacterPage} from "../CharacterPage/CharacterPage.jsx";
+import {LocationPage} from "../LocationPage/LocationPage.jsx";
+import {EpisodePage} from "../EpisodePage/EpisodePage.jsx";
 
 
 export const HomePage = () => {
     return (
-        <div>
-            <h1>The Rick and Morty</h1>
-        </div>
+        <>
+            <NavLink to="/characters">Characters</NavLink>
+            <NavLink to="/locations">Locations</NavLink>
+            <NavLink to="/episodes">Episodes</NavLink>
+        </>
     );
 };
-
