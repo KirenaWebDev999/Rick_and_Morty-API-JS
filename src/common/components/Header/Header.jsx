bc0f0e909c8edda34ee553f206e3dec8fa276665
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-13 23:25:19 +0200

    added logo

---
+++ src/common/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../../../assets/img/logo.png'
+
+export const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt="logotype"/>
+        </div>
+    );
+};
+
