4336435577323c313ae2d5891ce54b8b46adafac
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-13 23:10:18 +0200

    added Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import {HomePage} from "./pages/HomePage/HomePage.jsx";
 import {LocationPage} from "./pages/LocationPage/LocationPage.jsx";
 import {EpisodePage} from "./pages/EpisodePage/EpisodePage.jsx";
 import {CharacterPage} from "./pages/CharacterPage/CharacterPage.jsx";
+import {Header} from "./common/components/Header.jsx";
 
 
 function App() {
     return (
         <div>
+            <Header/>
             <HomePage/>
             <CharacterPage/>
             <LocationPage/>
