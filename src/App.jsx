3a079106fa45ae589bc4f6724b6cfa057b2fc141
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-13 23:30:43 +0200

    added Routers

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import {HomePage} from "./pages/HomePage/HomePage.jsx";
 import {LocationPage} from "./pages/LocationPage/LocationPage.jsx";
 import {EpisodePage} from "./pages/EpisodePage/EpisodePage.jsx";
 import {CharacterPage} from "./pages/CharacterPage/CharacterPage.jsx";
-import {Header} from "./common/components/Header.jsx";
+import {Header} from "./common/components/Header/Header.jsx";
+import {Route, Routes} from "react-router";
 
 
 function App() {
     return (
         <div>
             <Header/>
-            <HomePage/>
-            <CharacterPage/>
-            <LocationPage/>
-            <EpisodePage/>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/characters" element={<CharacterPage/>}/>
+                <Route path="/lacations" element={<LocationPage/>}/>
+                <Route path="/episodes" element={<EpisodePage/>}/>
+            </Routes>
         </div>
     )
 }
