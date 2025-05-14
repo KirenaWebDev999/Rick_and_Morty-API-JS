75f1971a7b376b5078093f3c0de071ba8d80a706
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-14 22:16:39 +0200

    added allEpisodes

--- src/pages/EpisodePage/EpisodePage.jsx
+++ src/pages/EpisodePage/EpisodePage.jsx
@@ -1,6 +1,15 @@
+import {useEffect} from "react";
+import axios from "axios";
 
 
 export const EpisodePage = () => {
+
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/episode').then((res) => {
+            console.log(res.data);
+        })
+    })
+
     return (
         <div>
             <h1 className={'pageTitle'}>EpisodePage</h1>
