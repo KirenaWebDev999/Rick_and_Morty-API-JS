cbe3b0fb51ce0bf3cba36ecad91f3262e8cf1ba9
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-15 00:27:32 +0200

    added map for episodesArr

--- src/pages/EpisodePage/EpisodePage.jsx
+++ src/pages/EpisodePage/EpisodePage.jsx
@@ -14,9 +14,25 @@ export const EpisodePage = () => {
     })
 
     return (
-        <div>
+        <div className={'pageContainer'}>
             <h1 className={'pageTitle'}>EpisodePage</h1>
-            <div>{episodes.length && episodes[0].name}</div>
+            {episodes.length && (
+                <div>
+                    {episodes.map((episode) => {
+                        return (
+                            <div key={episode.id}>
+                                <ul>
+                                    <li>Episode name: <b>{episode.name}</b></li>
+                                    <li>Date of episode aired: <b>{episode.air_date}</b></li>
+                                    <li>Episode: <b>{episode.episode}</b></li>
+                                    <li>List of characters who were seen in the episode: <b>{episode.characters.length}</b></li>
+                                </ul>
+                                <hr/>
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
         </div>
     );
 };
