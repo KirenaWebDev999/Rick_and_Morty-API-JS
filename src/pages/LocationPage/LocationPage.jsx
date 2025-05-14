971229a62ca4fdabc149d0a842ba35c128617b0b
KirenaWebDev999 <elena.job.984@gmail.com>
2025-05-14 22:19:18 +0200

    added allLocations

--- src/pages/LocationPage/LocationPage.jsx
+++ src/pages/LocationPage/LocationPage.jsx
@@ -1,6 +1,15 @@
+import {useEffect} from "react";
+import axios from "axios";
 
 
 export const LocationPage = () => {
+
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/location").then((res) => {
+            console.log(res.data);
+        })
+    })
+
     return (
         <div>
             <h1 className={'pageTitle'}>LocationPage</h1>
